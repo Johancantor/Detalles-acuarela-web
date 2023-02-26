284108e776fc28dd6ec22e1ccecc8f24ecaf192a
Johan Alexander Cantor Rodriguez <Cantor.johan89@gmail.com>
2023-02-26 00:16:10 -0500

    Animación js
    Animación en la page acerca de nosotros

---
+++ nosotros.js
@@ -0,0 +1,53 @@
+
+one.onclick = function() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+
+      one.style.bottom= timePassed / 5 + 'px';
+
+      if (timePassed > 2000) clearInterval(timer);
+
+    }, 20);
+  }
+
+two.onclick = function() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+
+      two.style.bottom= timePassed / 5 + 'px';
+
+      if (timePassed > 990) clearInterval(timer);
+
+    }, 20);
+
+}
+
+three.onclick = function() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+
+      three.style.bottom= timePassed / 5 + 'px';
+
+      if (timePassed > 1830) clearInterval(timer);
+
+    }, 20);
+}
+
+four.onclick = function() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+
+      four.style.bottom= timePassed / 5 + 'px';
+
+      if (timePassed > 990) clearInterval(timer);
+
+    }, 20);
+}
