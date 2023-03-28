3f45f083f4c9468f0d0858421c67ddf42c3502b1
Johancantor <123007884+Johancantor@users.noreply.github.com>
2023-03-27 22:40:25 -0500

    Creacion.
    Se crea el menu hamburguesa y se mejora responsive.

--- HTML/JS/MenuHam.js
+++ HTML/JS/MenuHam.js
@@ -1,11 +1,7 @@
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+let menu = document.querySelector(".menu");
 
-let line1__bars = document.querySelector(".line1__bars-menu");
-let line2__bars = document.querySelector(".line2__bars-menu");
-let line3__bars = document.querySelector(".line3__bars-menu");
-
-function animateBars(){
-  line1__bars.classList.toggle("activeline1__bars-menu");
-  line2__bars.classList.toggle("activeline2__bars-menu");
-  line3__bars.classList.toggle("activeline3__bars-menu");
+function toggleMenu() {
+  menu.classList.toggle("open");
 }
+
+document.querySelector(".bars__menu").addEventListener("click", toggleMenu);
